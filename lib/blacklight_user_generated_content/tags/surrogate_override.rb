837f2ffc86d610bea5ea164266e8c96aa1954321
Chris Beer <chris_beer@wgbh.org>
2010-10-15 16:12:17 -0400

    add _on field

--- lib/blacklight_user_generated_content/tags/surrogate_override.rb
+++ lib/blacklight_user_generated_content/tags/surrogate_override.rb
@@ -1,7 +1,7 @@
 module BlacklightUserGeneratedContent::Tags::SurrogateOverride
   def self.included(base)
     base.class_eval do
-      acts_as_taggable
+      acts_as_taggable_on :tags
     end
   end
 
