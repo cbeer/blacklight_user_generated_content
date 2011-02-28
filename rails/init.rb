4419fe3d1c44e0ec7667ff73cd152ecd8704a99c
Chris Beer <chris@cbeer.info>
2011-02-28 17:01:19 -0500

    acts as rateable is available as a gem

--- rails/init.rb
+++ rails/init.rb
@@ -3,9 +3,7 @@ require 'blacklight_user_generated_content'
 
 config.gem "acts-as-taggable-on", :source => "http://gemcutter.org", :version => '2.0.0.rc1'
 config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
-# Wouldn't it be nice if this was also distributed as a gem...
-# config.gem 'azabaj-acts_as_rateable', :lib => 'acts_as_rateable', :source => "http://gems.github.com"
-#
+config.gem "acts_as_rateable"
 
 
 # We do our injection in after_initialize so an app can stop it or configure
