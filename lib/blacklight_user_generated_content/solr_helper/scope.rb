ba4d3629274258c41d098a03c96c69e508e31f1e
Chris Beer <chris_beer@wgbh.org>
2011-06-06 11:34:56 -0400

    fix variable name

--- lib/blacklight_user_generated_content/solr_helper/scope.rb
+++ lib/blacklight_user_generated_content/solr_helper/scope.rb
@@ -8,7 +8,7 @@ module BlacklightUserGeneratedContent
       def restrict_to_user_generated_content_scope solr_parameters, user_parameters
         solr_parameters[:fq] ||= []
         if current_user
-          if current_user.has_role? :admin and user_params[:all]
+          if current_user.has_role? :admin and user_parameters[:all]
             solr_parameters[:fq] << "comments_ids_i:[* TO *]"
           else
             solr_parameters[:fq] << "comments_user_ids_i:#{current_user.id} OR tags_user_ids_i:#{current_user.id}"
