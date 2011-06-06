7b2cc8201797d5ca0c471565359c37e94c704641
Chris Beer <chris@cbeer.info>
2011-06-05 20:42:49 -0400

    add user generated content facet route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match "user_generated_content/facet/:id", :to => 'user_generated_content#facet'
   resources :user_generated_content, :only => [:index]
   resources :comments
   resources :tags
